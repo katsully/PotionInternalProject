a23490d51cd755454d55ef1411062143e72114f0
Kat Sullivan <kmsullivan012@gmail.com>
2015-06-17 15:13:39 -0400

    Have first version of motion tracking working

--- FrameSubtraction/src/FrameSubtractionApp.cpp
+++ FrameSubtraction/src/FrameSubtractionApp.cpp
@@ -16,46 +16,42 @@ using namespace ci::app;
 using namespace std;
 
 class FrameSubtractionApp : public AppNative {
-public:
+ public:
     void setup();
-    void keyDown( KeyEvent event );
     void prepareSettings( Settings* settings );
-    void update(openni::VideoFrameRef frame);
+    void update();
     void draw();
     
-    cv::BackgroundSubtractorMOG mBSubtractor;
-    vector< vector<cv::Point> > mContours;
+    cv::Mat mPreviousFrame;
+    vector< cv::Point2f > mPreviousFeautres, mFeatures;
+    vector< uint8_t > mFeatureStatuses;
     
-    bool mUpdateBackground;
-    gl::Texture mForegroundTexture, mBackgroundTexture;
     
-    int mDrawMode;
-private:
-    Channel16u mChannel;
+ private:
     OpenNI::DeviceRef mDevice;
     OpenNI::DeviceManagerRef mDeviceManager;
     gl::TextureRef mTexture;
+    gl::TextureRef mTextureDepth;
     
-    void onDepth( openni::VideoFrameRef frame, const OpenNI::DeviceOptions& deviceOptions );
+    ci::Surface8u mSurface;
+    ci::Surface8u mSurfaceDepth;
+    
+    void onDepth(openni::VideoFrameRef frame, const OpenNI::DeviceOptions& deviceOptions);
+    void onColor(openni::VideoFrameRef frame, const OpenNI::DeviceOptions& deviceOptions);
     void screenShot();
 };
 
 void FrameSubtractionApp::setup(){
-    // setup for frame subtraction
-    mDrawMode = 0;
-    // we want to start with the background updating
-    mUpdateBackground = true;
     
-    // make some textures to draw into
-    gl::Texture::Format fmt;
-    mForegroundTexture = gl::Texture(640, 480, fmt);
-    mBackgroundTexture = gl::Texture(640, 480, fmt);
+    // Setup the parameters
+//    mParams = params::INterfaceGl("Parameters", Vec2i(200, 150) );
+//    mParams.addParam( "Picked Color", &mPickedColor, "readonly=1" );
     
     mDeviceManager = OpenNI::DeviceManager::create();
     
     if( mDeviceManager->isInitialized() ){
         try{
-            mDevice = mDeviceManager->createDevice();
+            mDevice = mDeviceManager->createDevice( OpenNI::DeviceOptions().enableColor() );
         } catch ( OpenNI::ExcDeviceNotAvailable ex ){
             console() << ex.what() << endl;
             quit();
@@ -64,19 +60,10 @@ void FrameSubtractionApp::setup(){
         
         if( mDevice ){
             mDevice->connectDepthEventHandler( &FrameSubtractionApp::onDepth, this);
+            mDevice->connectColorEventHandler( &FrameSubtractionApp::onColor, this );
+            mPreviousFrame = cv::Mat( 240,320, CV_16UC1 );
             mDevice->start();
         }
-    }
-}
-
-void FrameSubtractionApp::onDepth( openni::VideoFrameRef frame, const OpenNI::DeviceOptions& deviceOptions ){
-    mChannel = OpenNI::toChannel16u( frame );
-}
-
-void FrameSubtractionApp::keyDown( KeyEvent event ){
-    mDrawMode++;
-    if(mDrawMode > 2){
-        mDrawMode = 0;
     }
 }
 
@@ -85,16 +72,48 @@ void FrameSubtractionApp::prepareSettings( Settings* settings ){
     settings->setWindowSize( 800, 600 );
 }
 
-void FrameSubtractionApp::update(openni::VideoFrameRef frame){
-    if( getElapsedFrames() % 120 ){
-        mUpdateBackground = true;
-    }
-
-    // frame from Video ref can be converted to surface to make this code work but you need to pass a parameter to the update method
+void FrameSubtractionApp::onDepth(openni::VideoFrameRef frame, const OpenNI::DeviceOptions& deviceOptions){
     
-    cv::Mat working, newFrame, fgFrame;
-    OpenNI::toSurface16u( frame );
-//    newFrame = toOcv(mTexture);
+    cv::Mat mInput = toOcv( OpenNI::toChannel16u( frame ) );
+    cv::Mat mInput8bit;
+    cv::Mat mSubtracted;
+    cv::Mat mThreshold;
+    cv::Mat mThresholdCopy;
+    
+    
+    
+    // subtrackted depth
+    cv::absdiff( mPreviousFrame, mInput, mSubtracted );
+    
+    mSubtracted.convertTo( mThreshold, CV_8UC1 );
+    cv::threshold( mThreshold, mThreshold, 100 ,0, cv::THRESH_BINARY );
+    
+    
+    
+    if ( ci::app::getElapsedFrames() % 200 )
+    {
+        cout << "my type " << mInput.type() << endl;
+        cout << "my channels " << mInput.channels() << endl;
+        cout << "my size " << mInput.size() << endl;
+        //cv::imwrite( "test_threshold.png" ,  mThreshold );
+    }
+    
+    // convert to RGB color space, with some compensation
+    //mInput.convertTo( mInput8bit, CV_8UC3, 0.1/1.0 );
+    mSubtracted.convertTo( mInput8bit, CV_8UC3, 0.1/1.0 );
+    
+    mInput.copyTo( mPreviousFrame );
+    
+    mSurfaceDepth = Surface8u( fromOcv( mInput8bit ) );
+}
+
+void FrameSubtractionApp::onColor(openni::VideoFrameRef frame, const OpenNI::DeviceOptions& deviceOptions){
+    mSurface = OpenNI::toSurface8u( frame );
+    cv::Mat mInput( toOcv( OpenNI::toSurface8u( frame), 0 ) );
+}
+
+void FrameSubtractionApp::update(){
+
 }
 
 void FrameSubtractionApp::draw()
@@ -102,14 +121,27 @@ void FrameSubtractionApp::draw()
     gl::setViewport( getWindowBounds() );
     // clear out the window with black
     gl::clear( Color( 0, 0, 0 ) );
+    if( mTexture ){
+        gl::color( Color::white() );
+        gl::draw( mTexture, getWindowBounds() );
+    }
     
-    if( mChannel ){
+    if( mSurface ){
         if( mTexture ){
-            mTexture->update( Channel32f( mChannel ) );
+            mTexture->update( mSurface );
         } else {
-            mTexture = gl::Texture::create( Channel32f( mChannel ) );
+            mTexture = gl::Texture::create( mSurface );
         }
         gl::draw( mTexture, mTexture->getBounds(), getWindowBounds() );
+    }
+    
+    if( mSurfaceDepth ){
+        if( mTextureDepth ) {
+            mTextureDepth->update( mSurfaceDepth );
+        } else {
+            mTextureDepth = gl::Texture::create( mSurfaceDepth );
+        }
+        gl::draw( mTextureDepth, mTextureDepth->getBounds(), getWindowBounds() );
     }
 }
 
