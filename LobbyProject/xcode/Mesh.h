79fdc347df20efd7a10f225a5ed648b2818e34e2
peterobbin <beijingwang@gmail.com>
2015-06-24 12:45:42 -0400

    update mesh

--- LobbyProject/xcode/Mesh.h
+++ LobbyProject/xcode/Mesh.h
@@ -1,58 +1,52 @@
 //
-//  VBOMesh.h
-//  LobbyProject
+//  Mesh.h
+//  MeshController
 //
-//  
+//  Created by Luobin Wang on 6/23/15.
 //
 //
 
-#include "Resources.h"
+#pragma once
 
-#include "cinder/app/AppBasic.h"
+
+
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Vbo.h"
 #include "cinder/gl/Texture.h"
-#include "cinder/Camera.h"
-#include "cinder/ImageIo.h"
-#include "cinder/params/Params.h"
-#include "cinder/Rand.h"
 #include "cinder/Perlin.h"
+
+
 
 using namespace ci;
 using namespace ci::app;
-
 using std::vector;
-params::InterfaceGl mParams;
 
-class VBOMesh {
+
+class Mesh{
 public:
-    VBOMesh();
-    void setup();
-    void update();
+    Mesh(int vertices_x, int vertices_y, gl::TextureRef &texture,  int meshType);
+    
+    void update(Vec2f &_mousePos);
     void draw();
-    void mouseMove(MouseEvent event);
     
-    static const int VERTICES_X = 50, VERTICES_Z = 50;
+    int VERTICES_X, VERTICES_Y;
     
-    gl::VboMeshRef	mVboMesh, mVboMesh2 , mVboMesh3;
-    gl::TextureRef	mTexture;
+    gl::VboMeshRef  mVboMesh;
+    gl::TextureRef  mTexture;
     
-    gl::TextureRef  revealImage;
-    
-    CameraPersp		mCamera;
-    Vec3f           mEye, mCenter, mUp;
-    Vec2f           mousePos, mouseRelPos;
-    Quatf mSceneRotation;
+    vector<float>   timeDiff;
+    vector<bool>    isTarget;
+    Vec2f           mousePos;
     Perlin          mPerlin;
     
-    float mappingMin, mappingMax;
-    float cameraVisionMin, cameraVisionMax;
-    float time , timeCount, prevTime;
-    bool showLastLayer, showFrontLayer;
-    
-    vector<float> timeDiff;
-    vector<bool>  isTarget;
-    vector<float> timeDiff2;
-    vector<bool>  isTarget2;
     
 };
+
+
+
+
+
+
+
+
+
