c36fc544cbae334dca4adab2a8538e36f619565b
Kat Sullivan <kmsullivan012@gmail.com>
2015-07-15 11:59:45 -0400

    removed unused project

--- FrameSubtraction/xcode/build/FrameSubtraction.build/Debug/FrameSubtraction.build/Objects-normal/i386/ParticleController.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/kathleen/PotionProject/FrameSubtraction/xcode/ParticleController.cpp \
-  /Users/kathleen/PotionProject/FrameSubtraction/xcode/ParticleController.h \
-  /Users/kathleen/PotionProject/FrameSubtraction/xcode/Particle.h \
-  ../../../Downloads/cinder_0.8.6_mac/include/cinder/Rand.h
